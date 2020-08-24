c57aef8bce440bbae48955aa902440e7ecfb7af7
Handdy <handdy.vargas@gmail.com>
2020-08-25 00:51:54 +0200

    imagenes y animar javascript

---
+++ src/js/animar.js
@@ -0,0 +1,15 @@
+let animado = document.querySelectorAll(".animado");
+
+function mostrarScroll()
+{
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i < animado.length; i++)
+    {
+        let alturaAnimado = animado[i].offsettop;
+        if(alturaAnimado - 300 < scrollTop)
+        {
+            animado[i].style.opacity = 1;
+        }
+    }
+}
+window.addEventListener('scroll', mostrarScroll);
